874cf67010de87684ddd0cc6dcf318d3ecaaf32e
Iino Ten <nteten0313@icloud.com>
2018-10-23 12:09:38 +0900

    表示されるバージョンの数値をクリックごとに増加させるようにした。そこまで理解はしていない

--- reactJS/react_tutorial/first_app/src/App.js
+++ reactJS/react_tutorial/first_app/src/App.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Title from './compornents/Title';  //読み込み
+import Title from './compornents/Title/Title';  //読み込み
 
 class App extends Component {
+
+  state = { version: '1.0' }
+
+  constructor(){
+    super()
+
+    this.state = {version:'1.0'}
+  }
+
+  
+
+  // イベントハンドラー
+  onClickHandler = () => {
+
+    // 「Hello World」のh2タグをここで取得
+    let nextVersion = parseInt(this.state.version, 10) + 1;
+    this .setState({ version: nextVersion.toFixed( 1 )})
+  }
+  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Title title="HelloWorld3.0" titleStyle={{color:'orange'}}/>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <Title
+            title="Hello World 3.0"
+            titleStyle={{color: 'orange'}}
+            onClick={this.onClickHandler}
+          >HelloWorld <span id="versionCounter" style={{borderBottom: '1px solid orange'}}>
+          {this.state.version}
+          </span>
+          </Title>
         </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
 }
+
 
 export default App;
--- reactJS/react_tutorial/first_app/src/compornents/Title/Title.js
+++ reactJS/react_tutorial/first_app/src/compornents/Title/Title.js
@@ -1,15 +1,28 @@
 //外部のライブラリやファイルを参照するインポートの部分
 import React from 'react';  //リアクトを使うときのおまじない的なやつ
+import './Title.css';
 
 // JSXやメソッドを定義する実質上の中身
 
 const Title = (props) => {
+
     return(
-        <div>
-            <h2 style={props.titleStyle}>{props.title}</h2>
-        </div>
+      <div className="titleSection">
+  
+        <h2
+          style={props.titleStyle}
+          id="versionStatement"
+        >{props.children}</h2>
+  
+        <p
+          onClick={props.onClick}
+          id="upgradeButton"
+          className="upgrade-button"
+        >Upgrade</p>
+  
+      </div>
     )
-}
+  }
 
 // 外部のファイルがこのcomponentを利用できるようにするエクスポートの部分
 export default Title;
