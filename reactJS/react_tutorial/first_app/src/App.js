adadb220975ef3fef1c732c0253c47d3fdf2ebdc
Iino Ten <nteten0313@icloud.com>
2018-10-26 14:07:17 +0900

    #7を完了させた

--- reactJS/react_tutorial/first_app/src/App.js
+++ reactJS/react_tutorial/first_app/src/App.js
@@ -25,26 +25,43 @@ class App extends Component {
   
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+    
 
-          <Title
-            title="Hello World 3.0"
-            titleStyle={{color: 'orange'}}
+		  let upgradeButton = (
+		    <p
             onClick={this.onClickHandler}
-          >HelloWorld <span id="versionCounter" style={{borderBottom: '1px solid orange'}}>
-          {this.state.version}
-          </span>
-          </Title>
+            id="upgradeButton"
+            className="upgrade-button"
+        	>Upgrade</p>
+		  );
+
+		  if ( this.state.version === '5.0'){
+			  upgradeButton = (
+				<p
+				className="upgraded-button"
+			  >Already up-to-date</p>
+			  );
+		  }
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
   }
+  return (
+	<div className="App">
+	  <header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+
+		<Title
+		  title="Hello World 3.0"
+		  titleStyle={{color: 'orange'}}
+		>HelloWorld <span id="versionCounter" style={{borderBottom: '1px solid orange'}}>
+		{this.state.version}
+		</span>
+		</Title>
+  );
 }
 
 
