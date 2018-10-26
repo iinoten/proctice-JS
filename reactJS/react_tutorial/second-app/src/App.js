ba94554304cf8e818ab38d96e3619ed030989fd7
Iino Ten <nteten0313@icloud.com>
2018-10-26 23:48:39 +0900

    #8のやつ終わった、youtubeのAPIからデータをっってきた

--- reactJS/react_tutorial/second-app/src/App.js
+++ reactJS/react_tutorial/second-app/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header'
+import YSearch from 'youtube-api-search';
+import Header from './components/Header/Header';
+
+
+const YOUTUBE_API_KEY = 'AIzaSyBSRz2siRGny5nRXeJoVIsY-J_d-QwiSWQ'
+
+YSearch({key:YOUTUBE_API_KEY, term:'猫　きゅうり'},(data) =>{
+  console.log(data);
+});
 
 class App extends Component {
   render() {
