be3bf00a2275d64b5a9fb6349b3892a59a370b60
Iino Ten <nteten0313@icloud.com>
2018-10-27 02:36:45 +0900

    #9の終わりかけくらいまでやりました。ライフサイクルをよく理解していない

--- reactJS/react_tutorial/second-app/src/App.js
+++ reactJS/react_tutorial/second-app/src/App.js
@@ -6,12 +6,37 @@ import Header from './components/Header/Header';
 
 const YOUTUBE_API_KEY = 'AIzaSyBSRz2siRGny5nRXeJoVIsY-J_d-QwiSWQ'
 
-YSearch({key:YOUTUBE_API_KEY, term:'猫　きゅうり'},(data) =>{
-  console.log(data);
-});
-
 class App extends Component {
+  state = { video:[]}
+
+  componentWillMount(){
+    console.log("compornentWillMountです")
+  }
+
+  componentDidMount(){
+    YSearch({key:YOUTUBE_API_KEY,term: '猫 きゅうり'},(data) => {
+      this.setState({videos:data});
+    });
+    console.log("componentDidMountです");
+  }
+
+  shouldComponentUpdate(nextProps, nextState){  //用途:そのcomponentが本当に更新されるべきか、更新される予定のpropsとstateを現在のpropsとstateと比較して決める。
+    console.log('更新前のstate' + this.state.videos);
+    console.log('更新後のstate' + nextState.videos);
+    return true;//んで多分trueかfalseか決めるんやとおもう。componentWillUpdateに影響する
+  }
+
+  componentWillUpdate(){  //直前のshouldComponentUpdateがtrueだったから動く
+    console.log("componentWillUpdateです")
+  }
+
+  componentDidUpdate(){
+    console.log("compnentDidUpdateです")
+  }
+
+
   render() {
+    console.log(this.state.videos);
     return (
       <div className="App">
         <Header />
